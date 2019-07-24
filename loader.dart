9d94db22baa14fc362bb9bad1b9cce3595176739
Michael Cockinos <michael.cockinos@icloud.com>
2019-07-24 18:49:42 +0800

    Add files via upload

---
+++ loader.dart
@@ -0,0 +1,179 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class Loader extends StatefulWidget {
+  @override
+  _LoaderState createState() => _LoaderState();
+}
+
+class _LoaderState extends State<Loader> with SingleTickerProviderStateMixin {
+  AnimationController controller;
+
+  Animation<double> animationRotation;
+  Animation<double> animationRadiusIn;
+  Animation<double> animationRadiusOut;
+
+  final double initialRadius = 50.0;
+  double radius;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+
+    animationRotation = Tween<double>(
+      begin: 1.0,
+      end: 0.0,
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(0.0, 1.0, curve: Curves.linear),
+      ),
+    );
+
+    animationRadiusIn = Tween<double>(
+      begin: 1.0,
+      end: 0.0,
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(0.75, 1.0, curve: Curves.elasticIn),
+      ),
+    );
+
+    animationRadiusOut = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(0.0, 0.25, curve: Curves.elasticOut),
+      ),
+    );
+
+    controller.addListener(() {
+      setState(() {
+        if (controller.value >= 0.75 && controller.value <= 1.0) {
+          radius = animationRadiusIn.value * initialRadius;
+        } else if (controller.value >= 0.0 && controller.value <= 0.25) {
+          radius = animationRadiusOut.value * initialRadius;
+        }
+      });
+    });
+
+    controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Center(
+        child: Stack(
+          children: <Widget>[
+            Center(
+              child: Image(
+                image: AssetImage('images/logo.png'),
+                width: 50.0,
+                height: 50.0,
+              ),
+            ),
+            Transform.translate(
+              offset: Offset(radius * cos(pi / 4), radius * sin(pi / 4)),
+              child: Dot(
+                radius: 10.0,
+                color: Colors.blue,
+              ),
+            ),
+            Transform.translate(
+              offset:
+                  Offset(radius * cos(2 * pi / 4), radius * sin(2 * pi / 4)),
+              child: Dot(
+                radius: 10.0,
+                color: Colors.green,
+              ),
+            ),
+            Transform.translate(
+              offset:
+                  Offset(radius * cos(3 * pi / 4), radius * sin(3 * pi / 4)),
+              child: Dot(
+                radius: 10.0,
+                color: Colors.yellow,
+              ),
+            ),
+            Transform.translate(
+              offset:
+                  Offset(radius * cos(4 * pi / 4), radius * sin(4 * pi / 4)),
+              child: Dot(
+                radius: 10.0,
+                color: Colors.orange,
+              ),
+            ),
+            Transform.translate(
+              offset:
+                  Offset(radius * cos(5 * pi / 4), radius * sin(5 * pi / 4)),
+              child: Dot(
+                radius: 10.0,
+                color: Colors.red,
+              ),
+            ),
+            Transform.translate(
+              offset:
+                  Offset(radius * cos(6 * pi / 4), radius * sin(6 * pi / 4)),
+              child: Dot(
+                radius: 10.0,
+                color: Colors.indigo,
+              ),
+            ),
+            Transform.translate(
+              offset:
+                  Offset(radius * cos(7 * pi / 4), radius * sin(7 * pi / 4)),
+              child: Dot(
+                radius: 10.0,
+                color: Colors.green,
+              ),
+            ),
+            Transform.translate(
+              offset:
+                  Offset(radius * cos(8 * pi / 4), radius * sin(8 * pi / 4)),
+              child: Dot(
+                radius: 10.0,
+                color: Colors.green,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Dot extends StatelessWidget {
+  final double radius;
+  final Color color;
+
+  const Dot({Key key, this.radius, this.color}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: this.radius,
+        height: this.radius,
+        decoration: BoxDecoration(color: this.color, shape: BoxShape.circle),
+      ),
+    );
+  }
+}
